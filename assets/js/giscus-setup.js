f7f7ebc6590b6d63f9901f1cd8e9e03ea13c0aa8
AndrejPer <43759884+AndrejPer@users.noreply.github.com>
2026-01-03 15:46:16 +0000

    Deploying to gh-pages from @ AndrejPer/demo_AndrejPer.github.io@8e3f60e4e778f0535f0a6292e9eebdfb90d6aff9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AndrejPer/AndrejPer.github.io",
+    "data-repo": "AndrejPer/demo_AndrejPer.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
